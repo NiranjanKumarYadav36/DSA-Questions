06d21334f8e603d934200b19390c7b6cf2bbad75
Niranjan Kumar Yadav <140634765+NiranjanKumarYadav36@users.noreply.github.com>
2023-12-12 14:31:45 +0530

    Update 62_27_niranjan.c

--- 62_27_niranjan.c
+++ 62_27_niranjan.c
@@ -5,113 +5,97 @@
   Description: progarmm id 27
 */
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
- int q[ 20 ], front = -1, rear = -1, vis[ 20 ];
- int a[20][20];
+int q[20], front = -1, rear = -1, vis[20];
+int a[20][20];
 
- int delete();
- void add ( int item );
-  
- void bfs( int s, int n );
+int delete();
+void add(int item);
+void bfs(int s, int n);
 
-void main()
-{
-    int n, i, s,j;
+int main() {
+    int n, i, s, j;
 
-    
-	// clrscr();
-    
-	printf( "ENTER THE NUMBER VERTICES " );
-    scanf( "%d", &n );
+    // clrscr();
 
-    for ( i = 1;i <= n;i++ )
-	{
-	    for ( j = 1;j <= n;j++ )
-		{
-		    printf( "ENTER 1 IF %d HAS A NODE WITH %d ELSE 0 ", i, j );
-		    scanf( "%d", &a[ i ][ j ] );
-		}
-	}
+    printf("ENTER THE NUMBER VERTICES ");
+    scanf("%d", &n);
 
-    printf( "THE ADJACENCY MATRIX IS\n" );
-    for ( i = 1;i <= n;i++ )
-	{
-	    for ( j = 1;j <= n;j++ )
-		{
-		    printf( " %d", a[ i ][ j ] );
-		}
-	    printf( "\n" );
-	}
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf("ENTER 1 IF %d HAS A NODE WITH %d ELSE 0 ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
 
-	    for ( i = 1;i <= n;i++ )
-		vis[ i ] = 0;
+    printf("THE ADJACENCY MATRIX IS\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf(" %d", a[i][j]);
+        }
+        printf("\n");
+    }
 
-	    printf( "ENTER THE SOURCE VERTEX :" );
-	    scanf( "%d", &s );
-		
-		// bfs (source_node, total_nodes)
-		bfs( s, n );
-// getch();
+    for (i = 1; i <= n; i++)
+        vis[i] = 0;
+
+    printf("ENTER THE SOURCE VERTEX :");
+    scanf("%d", &s);
+
+    bfs(s, n);
+
+    return 0;
 }
 
-void bfs( int s, int n )
-{
+void bfs(int s, int n) {
     int p, i;
-	add( s );
-	vis[ s ] = 1;
+    add(s);
+    vis[s] = 1;
 
     p = delete();
 
-    if ( p != 0 )
-	printf( " %d", p );
+    if (p != 0)
+        printf(" %d", p);
 
-    while ( p != 0 )
-	{
-	    for ( i = 1;i <= n;i++ )
-		if ( ( a[ p ][ i ] != 0 ) && ( vis[ i ] == 0 ) )
-		    {
-			add( i );
+    while (p != 0) {
+        for (i = 1; i <= n; i++)
+            if ((a[p][i] != 0) && (vis[i] == 0)) {
+                add(i);
+                vis[i] = 1;
+            }
 
-			vis[ i ] = 1;
-		    }
+        p = delete();
 
-	    p = delete();
-
-	    if ( p != 0 )
-		printf( " %d ", p );
-	}
-
-    for ( i = 1;i <= n;i++ )
-		if ( vis[ i ] == 0 )
-	    	bfs( i, n );
-}
-
-   void add( int item )
-    {
-	if ( rear == 19 )
-	    printf( "QUEUE FULL" );
-	else
-	    {
-		if ( rear == -1 )
-		    {
-			q[ ++rear ] = item;
-			front++;
-		    }
-		else
-		    q[ ++rear ] = item;
-	    }
+        if (p != 0)
+            printf(" %d ", p);
     }
 
-    int delete()
-    {
-     int k;
+    for (i = 1; i <= n; i++)
+        if (vis[i] == 0)
+            bfs(i, n);
+}
 
-     if ( ( front > rear ) || ( front == -1 ) )
-		return ( 0 );
-      else
-	{
-	    k = q[ front++ ];
-	    return ( k );
-	}
-     }
+void add(int item) {
+    if (rear == 19)
+        printf("QUEUE FULL");
+    else {
+        if (rear == -1) {
+            q[++rear] = item;
+            front++;
+        } else
+            q[++rear] = item;
+    }
+}
+
+int delete() {
+    int k;
+
+    if ((front > rear) || (front == -1))
+        return (0);
+    else {
+        k = q[front++];
+        return (k);
+    }
+}
